27a247637685a0095758183678e8b6ebae0eaf7a
oufresh <sergio.gennari@softeco.it>
2021-05-03 17:30:59 +0200

    ttt

--- hello/src/generic/traits.rs
+++ hello/src/generic/traits.rs
@@ -6,10 +6,19 @@
 /// Trait definitions are a way to group method signatures together to define a set of behaviors necessary to accomplish some purpose.
 /// 
 
-pub trait Summary {
+/*pub trait Summary {
     fn summarize(&self) -> String;
-}
+}*/
 
+/// trait with default
+pub trait Summary {
+    fn summarize_author(&self) -> String {
+        String::from("(Read more on author...)")
+    }
+    fn summarize(&self) -> String {
+        String::from("(Read more...)")
+    }
+}
 pub struct NewsArticle {
     pub headline: String,
     pub location: String,
@@ -34,19 +43,58 @@ impl Summary for Tweet {
     fn summarize(&self) -> String {
         format!("{}: {}", self.username, self.content)
     }
+    fn summarize_author(&self) -> String {
+        format!("@{}", self.username)
+    }
 }
 
+pub fn notify<T: Summary>(item: &T) {
+    println!("Breaking news! {}", item.summarize());
+}
 
-/////////
-pub fn test_trait_summarize() {
-    let tweet = Tweet {
+/* multiple traits
+pub fn notify<T: Summary>(item1: &T, item2: &T) {
+...}
+ 
+pub fn notify<T: Summary + Display>(item: &T) {
+...
+/}
+
+// where syntax 
+ fn some_function<T, U>(t: &T, u: &U) -> i32
+where T: Display + Clone,
+U: Clone + Debug
+{
+
+**/
+
+fn returns_summarizable() -> impl Summary {
+    Tweet {
         username: String::from("horse_ebooks"),
         content: String::from(
             "of course, as you probably already know, people",
         ),
         reply: false,
         retweet: false,
-    };
+    }
+}
+/////////
+pub fn test_trait_summarize() {
+    let tweet = returns_summarizable();
 
-    println!("1 new tweet: {}", tweet.summarize());
+    println!("");
+
+    let article = NewsArticle {
+        headline: String::from("Penguins win the Stanley Cup Championship!"),
+        location: String::from("Pittsburgh, PA, USA"),
+        author: String::from("Iceburgh"),
+        content: String::from(
+            "The Pittsburgh Penguins once again are the best \
+             hockey team in the NHL.",
+        ),
+    };
+    println!("New article available trait default!");
+    notify(&article);
+    println!("1 new tweet");
+    notify(&tweet);
 }
